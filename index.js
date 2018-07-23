a4e6619bd6231e37cf15ec33bd5393bf603992b8
Ravi448 <jarvicephp@gmail.com>
2018-07-24 00:17:09 +0530

    initial commits

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-export * from './src/upload'
+import {singleTransfer} from './src/functions/commons'
+module.exports.singleFile =  function(){
+    console.log(singleTransfer())
+};
--- src/upload.js
+++ src/upload.js
@@ -1,4 +1,4 @@
 import {singleTransfer} from './functions/commons'
 module.exports.singleFile =  function(){
-    console.log(this.singleFile())
+    console.log(singleTransfer())
 };
